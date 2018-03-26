b9df79ad50d3521adac41554ba7a8c8cb0c8ee5e
kishore routhu <kishore.routhu@wavemaker.com>
2018-03-26 15:35:08 +0530

    Added .gitignore file

---
+++ FirstApp/src/main/java/com/css/spring/boot/FirstApp.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class FirstApp {
+
+   @RequestMapping("/")
+   String home() {
+	return "Spring boot first app welcomes you...!";
+   }
+
+  public static void main(String args[]) throws Exception {
+	//SpringApplication.run(FirstApp.class, args);
+      SpringApplication application = new SpringApplication(FirstApp.class);
+      //application.setBannerMode(Banner.Mode.OFF);
+      application.run(args);
+  }
+}
